b85a31aab2c51948cc0a3d9874502d67f8a2c459
Tsubasa2540 <tsubasa2540@gmail.com>
2020-08-16 12:35:52 +0700

    init

---
+++ build/precache-manifest.ffaa18aa02411d8e42249a9c5cd12fe6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e6a4b7fac8fb9811ad0ddefdf83d23c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed20a4a082dd224f5bcc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7de02b121de3519ec53d",
+    "url": "/static/js/2.9a4c53a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9a4c53a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed20a4a082dd224f5bcc",
+    "url": "/static/js/main.62c37eb7.chunk.js"
+  },
+  {
+    "revision": "0288d39075dd47e70b5e",
+    "url": "/static/js/runtime-main.836deb25.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
